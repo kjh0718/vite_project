fa65d564362ddd92ae0bc33210f516e5506ff2ff
kjh0718 <ranbow3062961@gmail.com>
2026-05-10 13:01:22 +0900

    1~2

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+*{
+  box-sizing:border-box;
+  }
+  ${reset};
+  body{
+    color:white;
+    background-color:black;
+    font-family:system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+`;
+
+function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOAyXPBDLIjFTe8lXPGTyo5kQlyz0f1gE",
+  authDomain: "vite-project-38783.firebaseapp.com",
+  projectId: "vite-project-38783",
+  storageBucket: "vite-project-38783.firebasestorage.app",
+  messagingSenderId: "354008918192",
+  appId: "1:354008918192:web:408d38bd86cd632a3a3375"
+};
+
+const app = initializeApp(firebaseConfig);
